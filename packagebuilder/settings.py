81bea7fc22c768e73e1a84c4663693dbba867e11
Ben Edwards <bedwards@tquila.com>
2014-06-02 23:50:24 +0100

    Installed requests

--- packagebuilder/settings.py
+++ packagebuilder/settings.py
@@ -73,7 +73,7 @@ DATABASES['default'] =  dj_database_url.config()
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
