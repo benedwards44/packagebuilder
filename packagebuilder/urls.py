387f4d4319d1b4dffcfa30b344590b241b4cb3b5
Ben Edwards <bedwards@tquila.com>
2014-06-03 22:35:00 +0100

    Update static url references

--- packagebuilder/urls.py
+++ packagebuilder/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.contrib import admin
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
