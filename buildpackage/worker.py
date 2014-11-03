889cce43f2885690b8cffd71842a296d9d969340
Ben Edwards <bedwards@tquila.com>
2014-11-03 13:30:57 +0000

    Fix worker.py

--- buildpackage/worker.py
+++ buildpackage/worker.py
@@ -5,7 +5,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://redistogo:e777ce746dac90d65d926e755bcc1f8f@dab.redistogo.com:9006/')
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
 
