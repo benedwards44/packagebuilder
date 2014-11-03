7a4afb1fceb857076577035b19f3f100ccab6ece
Ben Edwards <bedwards@tquila.com>
2014-11-03 17:40:22 +0000

    Updated default timeout

--- packagebuilder/settings.py
+++ packagebuilder/settings.py
@@ -72,6 +72,7 @@ RQ_QUEUES = {
     'default': {
         'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379'), # If you're on Heroku
         'DB': 0,
+        'DEFAULT_TIMEOUT': 1000
     }
 }
 
