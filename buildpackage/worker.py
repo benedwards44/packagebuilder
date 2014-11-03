29020b774b0f8b79add8677bcab7025a7ce93cf6
Ben Edwards <bedwards@tquila.com>
2014-11-03 13:14:45 +0000

    Fix worker.py

--- buildpackage/worker.py
+++ buildpackage/worker.py
@@ -1,15 +1,18 @@
 import os
-
-import redis
+import urlparse
+from redis import Redis
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDISTOGO_URL')
+if not redis_url:
+    raise RuntimeError('Set up Redis To Go first.')
 
-conn = redis.from_url(redis_url)
+urlparse.uses_netloc.append('redis')
+url = urlparse.urlparse(redis_url)
+conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
-if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(map(Queue, listen))
-        worker.work()
+with Connection(conn):
+    worker = Worker(map(Queue, listen))
+    worker.work()
