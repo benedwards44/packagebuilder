290f772b014a7db3a74599798b4719f4eedb0456
Ben Edwards <bedwards@tquila.com>
2014-11-04 10:23:50 +0000

    Removed ajax.py

--- buildpackage/ajax.py
+++
@@ -1,9 +0,0 @@
-from django.utils import simplejson
-from dajax.core import Dajax
-from dajaxice.decorators import dajaxice_register
-
-@dajaxice_register
-def randomize(request):
-    dajax = Dajax()
-    dajax.assign('#result', 'value', random.randint(1, 10))
-    return dajax.json()
