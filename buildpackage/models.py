237cb9987d10aaaff817f64d872f9459c0dce3b9
Ben Edwards <bedwards@tquila.com>
2014-11-06 14:10:06 +0000

    Update DB

--- buildpackage/models.py
+++ buildpackage/models.py
@@ -4,8 +4,8 @@ class Package(models.Model):
 	username = models.CharField(max_length=255)
 	api_version = models.CharField(max_length=255)
 	package = models.TextField(blank=True)
-	status = models.CharField(max_length=255, null=True)
-	error = models.TextField(null=True)
+	status = models.CharField(max_length=255, blank=True)
+	error = models.TextField(blank=True)
 
 	def sorted_component_types(self):
 		return self.componenttype_set.order_by('name')
