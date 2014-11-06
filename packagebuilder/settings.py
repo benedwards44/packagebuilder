e9251b30ee38d3a4b2949da2bb5d36fc8e9c437f
Ben Edwards <bedwards@tquila.com>
2014-11-06 14:28:39 +0000

    Remove debug

--- packagebuilder/settings.py
+++ packagebuilder/settings.py
@@ -20,7 +20,7 @@ PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'pi3zx!cfom!wxjr4ti_z$l$d@_&rqbe@$4voa)#q2#kn$(8lto'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 THUMBNAIL_DEBUG = DEBUG
 
