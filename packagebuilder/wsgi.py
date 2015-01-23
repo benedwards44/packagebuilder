f86b6891c7040f82f048c1ea5ca238e5faeb18d4
Ben Edwards <bedwards@tquila.com>
2015-01-23 12:44:57 +0000

    Fix static resources

--- packagebuilder/wsgi.py
+++ packagebuilder/wsgi.py
@@ -25,7 +25,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "packagebuilder.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
