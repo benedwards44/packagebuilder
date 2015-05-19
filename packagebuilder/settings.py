38dd1b3c82a46447c1348bfc4cb71588a7943bb0
Ben Edwards <bedwards@tquila.com>
2015-05-19 12:56:31 +0100

    Enable debug

--- packagebuilder/settings.py
+++ packagebuilder/settings.py
@@ -55,8 +55,6 @@ MIDDLEWARE_CLASSES = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.template.context_processors.debug',
     'django.template.context_processors.request',
 )
 
