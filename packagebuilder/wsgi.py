519bd483776322ca9dcc46625b342150928e302f
Ben Edwards <ben@edwards.nz>
2018-01-26 15:38:14 +1300

    Now static files have stopped working!

--- packagebuilder/wsgi.py
+++ packagebuilder/wsgi.py
@@ -28,7 +28,3 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
