691978feee786615a0c6918a16b078f003c84f40
Ben Edwards <ben@edwards.nz>
2018-01-26 15:43:31 +1300

    Fix API

--- buildpackage/views.py
+++ buildpackage/views.py
@@ -339,7 +339,7 @@ def api_create_job(request):
             return HttpResponse(
                 json.dumps({
                     'success': True,
-                    'id': job.random_id
+                    'id': package.random_id
                 }),
                 content_type='application/json',
                 status=200
