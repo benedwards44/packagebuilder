49cd327f28ee1fd3910301838482ce3235dd9b6b
Ivanna Panchenko <ivanapanchenko@gmail.com>
2019-10-30 11:21:59 +1300

    Force push

--- packagebuilder/settings.py
+++ packagebuilder/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
