7331eff8020da901e33bf9f588b6b547489bd06e
Ben Edwards <ben@edwards.nz>
2019-11-04 15:50:31 +1300

    Cleaning up again

--- packagebuilder/static/js/main.js
+++ packagebuilder/static/js/main.js
@@ -1,18 +1,30 @@
 $(document).ready(function() {
 
-	$('.loading').hide();
-	$('.logging_out').hide();
+    $('.loading').hide();
+    $('.logging_out').hide();
 });
 
+function hideTable(){
+    $('.login_table').hide();
+    $('.messages').hide();
+    $('.loading').show();
+}
+function showTable(){
+    $('.login_table').show();
+    $('.messages').show();
+    $('.loading').hide();
+}
 
 function showLogout(){
-	$('.login_table').hide();
-	$('.messages').hide();
-	$('.logging_out').show();
+    $('.login_table').hide();
+    $('#select_components').hide();
+    $('.messages').hide();
+    $('.logging_out').show();
 }
 
 function hideLogout(){
-	$('.login_table').show();
-	$('.messages').show();
-	$('.logging_out').hide();
+    $('.login_table').show();
+    $('#select_components').show();
+    $('.messages').show();
+    $('.logging_out').hide();
 }
