8685b9decef709128306d3989240d049f8d34366
Ben Edwards <ben@edwards.nz>
2019-11-04 16:10:52 +1300

    Ben's stuff

--- buildpackage/forms.py
+++ buildpackage/forms.py
@@ -2,24 +2,23 @@ from django import forms
 from django.forms import ModelForm
 from buildpackage.models import Package
 
-
 class LoginForm(forms.Form):
-    environment = forms.CharField(required=False)
-    access_token = forms.CharField(required=False)
-    instance_url = forms.CharField(required=False)
-    org_id = forms.CharField(required=False)
 
-    # Options for including packages or not
-    PACKAGE_CHOICES = (
-        ('wildcard_only', 'Wildcard Only'),
-        ('all', 'All Components'),
-        ('unmanaged', 'Exclude Managed'),
-        ('none', 'No Packaged Components'),
+	environment = forms.CharField(required=False)
+	access_token = forms.CharField(required=False)
+	instance_url = forms.CharField(required=False)
+	org_id = forms.CharField(required=False)
 
-    )
+	# Options for including packages or not
+	PACKAGE_CHOICES = (
+		('wildcard_only', 'Wildcard Only'),
+		('all','All Components'),
+		('unmanaged','Exclude Managed'),
+		('none','No Packaged Components'),
+	)
 
-    package_option = forms.ChoiceField(choices=PACKAGE_CHOICES, required=False)
-
+	package_option = forms.ChoiceField(choices=PACKAGE_CHOICES, required=False)
 
 class ComponentSelectForm(forms.Form):
-    component_option = forms.CharField(required=False)
+	component_option = forms.CharField(required=False)
+
