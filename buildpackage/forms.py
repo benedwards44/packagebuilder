c4475d89db0faa34c506f2381ca16a1a69a6aa37
Ben Edwards <ben@edwards.nz>
2019-11-04 16:11:54 +1300

    Fixed indenting

--- buildpackage/forms.py
+++ buildpackage/forms.py
@@ -4,21 +4,21 @@ from buildpackage.models import Package
 
 class LoginForm(forms.Form):
 
-	environment = forms.CharField(required=False)
-	access_token = forms.CharField(required=False)
-	instance_url = forms.CharField(required=False)
-	org_id = forms.CharField(required=False)
+    environment = forms.CharField(required=False)
+    access_token = forms.CharField(required=False)
+    instance_url = forms.CharField(required=False)
+    org_id = forms.CharField(required=False)
 
-	# Options for including packages or not
-	PACKAGE_CHOICES = (
-		('wildcard_only', 'Wildcard Only'),
-		('all','All Components'),
-		('unmanaged','Exclude Managed'),
-		('none','No Packaged Components'),
-	)
+    # Options for including packages or not
+    PACKAGE_CHOICES = (
+        ('wildcard_only', 'Wildcard Only'),
+        ('all','All Components'),
+        ('unmanaged','Exclude Managed'),
+        ('none','No Packaged Components'),
+    )
 
-	package_option = forms.ChoiceField(choices=PACKAGE_CHOICES, required=False)
+    package_option = forms.ChoiceField(choices=PACKAGE_CHOICES, required=False)
 
 class ComponentSelectForm(forms.Form):
-	component_option = forms.CharField(required=False)
+    component_option = forms.CharField(required=False)
 
