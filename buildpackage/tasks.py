f1b2c0aecb650e91556dfb685c2bb81b9dc6ae82
Ben Edwards <ben@edwards.nz>
2019-11-04 16:03:04 +1300

    Fixed bug

--- buildpackage/tasks.py
+++ buildpackage/tasks.py
@@ -179,10 +179,10 @@ def query_components_from_org(package):
 
             # Run the metadata query only if the list has reached 3 (the max allowed to query)
             # at one time, or if there is less than 3 components left to query 
-            if component_list and len(component_list) == 3 or (len(all_metadata[0]) - loop_counter) <= 3:
+            if not is_wildcard and component_list and len(component_list) == 3 or (len(all_metadata[0]) - loop_counter) <= 3:
 
                 # loop through the components returned from the component query
-                for component in metadata_client.service.listMetadata(component_list,api_version):
+                for component in metadata_client.service.listMetadata(component_list, api_version):
 
                     # Query database for parent component_type
                     component_type_query = ComponentType.objects.filter(name=component.type, package=package.id)
