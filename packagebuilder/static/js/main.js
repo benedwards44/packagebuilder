1450fe15605cac92e03c4cd8818b36e0238b2398
Ben Edwards <ben@edwards.nz>
2019-11-04 15:47:34 +1300

    Cleanup

--- packagebuilder/static/js/main.js
+++ packagebuilder/static/js/main.js
@@ -1,66 +1,7 @@
 $(document).ready(function() {
 
 	$('.loading').hide();
-
 	$('.logging_out').hide();
-
-	$('#selection_tree').tree({
-		onCheck: {
-            node: 'expand'
-        },
-        onUncheck: {
-            node: 'collapse'
-        }
-	}); 
-
-	$('#selectAll').prop('checked', true);
-
-	$('#selection_tree input[type="checkbox"]').prop('checked', true);
-
-	$('#id_component_option').val('all');
-
-	$('#selection_tree').hide();
-
-	$('input[name="select"]').change(function() {
-		if ($(this).val() == 'partial') {
-			$('#selection_tree').show();
-			$('#id_component_option').val('partial');
-		} else {
-			$('#selection_tree').hide();
-			$('#id_component_option').val('all');
-		}
-	});		
-
-	$('.component_type_display').change(function() {
-
-		var component_id = $(this).attr('name');
-
-		if( $(this).prop('checked') ){
-			$('#id_component_type-' + component_id + '-include_all').prop('checked', true);
-		} else {
-			$('#id_component_type-' + component_id + '-include_all').prop('checked', false);
-		}
-
-	});
-
-	$('.component_display').change(function() {
-
-		var component_id = $(this).attr('name');
-		var checkbox_id = $('input[value="' + component_id + '"]').attr('id');
-		
-		checkbox_id = checkbox_id.replace('-id','-include');
-
-		if( $(this).prop('checked') ){
-			$('#'+checkbox_id).prop('checked', true);
-		} else {
-			$('#'+checkbox_id).prop('checked', false);
-		}
-
-	});
-
-	$('.select-component-options').show();
-	$('.loading-component-selector').hide();
-	
 });
 
 
