c310d4023ac544b79d9fb3e1a99f66aaafe129a7
Ben Edwards <ben@edwards.nz>
2023-06-01 20:13:08 +1200

    Git management command

--- buildpackage/management/commands/delete_packages.py
+++ buildpackage/management/commands/delete_packages.py
@@ -1,10 +1,10 @@
-from django.core.management.base import NoArgsCommand, CommandError, BaseCommand
+from django.core.management.base import BaseCommand
 from buildpackage.models import Package
 import datetime
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
 
-    def handle_noargs(self, **options):
+    def handle(self, *args, **options):
         
         one_hour_ago = datetime.datetime.now() - datetime.timedelta(minutes=60)
         packages = Package.objects.filter(finished_date__lt = one_hour_ago)
